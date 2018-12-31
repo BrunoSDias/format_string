0a279d73f7dfa6883a05bb287b7a8056ac41ba0c
Bruno Dias <brunosdias@outlook.com>
2018-12-31 22:24:22 +0000

    gem finalizada

--- spec/format_string_spec.rb
+++ spec/format_string_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe FormatString do
-  it "has a version number" do
+  it "Verificar se versionamento está preenchido" do
     expect(FormatString::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "testa formatação em real" do
+    expect(FormatString::Moeda.br(2)).to eq("R$2")
+  end
+
+  it "testa formatação em dollar" do
+    expect(FormatString::Moeda.en(3)).to eq("$3")
   end
 end
